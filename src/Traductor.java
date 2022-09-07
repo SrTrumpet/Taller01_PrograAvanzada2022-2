a8aaa24666b7a8af1a2c2799bee52234b6a3c1e7
Elias Manque <elias.manque@alumnos.ucn.cl>
2022-09-07 16:04:40 -0300

    Update

--- src/Traductor.java
+++ src/Traductor.java
@@ -2,7 +2,10 @@
 public class Traductor {
 	
 	
-	
+	/***
+	 * Traduce el lenguaje extraterrestre a humano
+	 * @param linea
+	 */
 	public void traductorExtraterrestreHumano(String linea) {
 		
 	}
